acbb24f1d72b6926adeef66b50025ca0eb2e92cd
ptrompeter <patrick.trompeter@gmail.com>
2017-11-16 13:26:06 -0800

    added route configuration to routes.py.

--- Blog2017/Blog2017/routes.py
+++ Blog2017/Blog2017/routes.py
@@ -1,3 +1,10 @@
+"""Routes for blog access and login."""
+
+
 def includeme(config):
+    """Included routes are picked up by the configurator."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('blog', '/blog/{id:\d+}/{slug}')
+    config.add_route('blog_action', '/blog/{action}')
+    config.add_route('auth', '/sign/{action}')
