09da1fc07b762637254425977a7f8545d9da5c91
ptrompeter <patrick.trompeter@gmail.com>
2017-11-16 15:35:03 -0800

    Created blog record service in service directory, updated blog record model with slug and created_in_words properties.

--- Blog2017/Blog2017/models/blog_record.py
+++ Blog2017/Blog2017/models/blog_record.py
@@ -8,6 +8,8 @@ from sqlalchemy import (
     UnicodeText,
     DateTime,
 )
+from webhelpers2.date import distance_of_time_in_words
+from webhelpers2.text import urlify
 
 
 class BlogRecord(Base):
@@ -19,3 +21,14 @@ class BlogRecord(Base):
     body = Column(UnicodeText, default=u'')
     created = Column(DateTime, default=datetime.datetime.utcnow)
     edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @property
+    def slug(self):
+        """Create a URL slug to be inserted into paths."""
+        return urlify(self.title)
+
+    @property
+    def created_in_words(self):
+        """Return a string describing how long ago an entry was created."""
+        return distance_of_time_in_words(self.created,
+                                         datetime.datetime.utcnow())
