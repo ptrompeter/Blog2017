8e0355e6e03aea8fa85b4e6ae5466fb9a9ced491
ptrompeter <patrick.trompeter@gmail.com>
2017-11-17 11:58:34 -0800

    Added basic templating for blog view and blog articles.

--- Blog2017/Blog2017/views/blog.py
+++ Blog2017/Blog2017/views/blog.py
@@ -1,17 +1,24 @@
+from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 from pyramid.view import view_config
+from ..models.blog_record import BlogRecord
+from ..services.blog_record import BlogRecordService
 
 
 @view_config(route_name='blog',
              renderer='Blog2017:templates/view_blog.jinja2')
 def blog_view(request):
-    """This route returns the basic blog view."""
-    return {}
+    """Return the basic blog view."""
+    blog_id = int(request.matchdict.get('id', -1))
+    entry = BlogRecordService.by_id(blog_id, request)
+    if not entry:
+        return HTTPNotFound()
+    return {'entry': entry}
 
 
 @view_config(route_name='blog_action', match_param='action=create',
              renderer='Blog2017:templates/edit_blog.jinja2')
 def blog_create(request):
-    """This route handles requests to add new records."""
+    """Handle requests to add new records."""
     return {}
 
 
